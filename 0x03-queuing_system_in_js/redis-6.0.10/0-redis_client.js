1604b24183d5d086200d6c074c2a7728e9ace157
thhvvv <makausilvia964@gmail.com>
2024-09-27 11:59:38 +0000

    tasks

---
+++ 0x03-queuing_system_in_js/redis-6.0.10/0-redis_client.js
@@ -0,0 +1,12 @@
+#!/user/bin/yarn dev
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client connected to the server', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
